0ec49f01bc93d526b5484a22d6feaab492dc2bf2
|||LOM||| <louis.olivier.major@gmail.com>
2024-03-06 09:58:49 -0500

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CharacterSheetDnD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
