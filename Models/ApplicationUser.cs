bbd7777256710fef39357dff84c752902516e828
|||LOM||| <louis.olivier.major@gmail.com>
2024-03-07 10:20:34 -0500

    Add StringLength(50) to FirstName and LastName

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace CharacterSheetDnD.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [StringLength(50)]
         public string FirstName { get; set; } = string.Empty;
+        [StringLength(50)]
         public string LastName { get; set; } = string.Empty;
     }
 }
