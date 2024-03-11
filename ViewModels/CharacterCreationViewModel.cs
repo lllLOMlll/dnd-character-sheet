7b5525ab4bc80e08fca3600175bc43cf38238461
|||LOM||| <louis.olivier.major@gmail.com>
2024-03-11 19:38:45 -0400

    Initial commit

---
+++ ViewModels/CharacterCreationViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CharacterSheetDnD.Models;
+
+namespace CharacterSheetDnD.ViewModels
+{
+    public class CharacterCreationViewModel
+    {
+        public Character Character { get; set; }
+        public CharacterClass CharacterClass { get; set; }
+        public CharacterStatistic CharacterStatistic { get; set; }
+        //public CharacterHealth characterHealth { get; set; }
+    }
+}
