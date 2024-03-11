f6584fa3608c5a0d8dea05a067ab10ed2c16a26d
|||LOM||| <louis.olivier.major@gmail.com>
2024-03-11 19:36:08 -0400

    Initial commit

---
+++ Models/CharacterHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CharacterSheetDnD.Models
+{
+    public class CharacterHealth
+    {
+        [Key]
+        public int HealthID { get; set; }
+
+        [Required]
+        public int MaximumHitPoints { get; set; }
+
+        [Required]
+        public int CurrentHitPoints { get; set; }
+
+        public int TemporaryHitPoints { get; set; }
+
+        [ForeignKey("Character")]
+        public int CharacterID { get; set; }
+
+        // Navigation property back to the Character
+        public virtual Character? Character { get; set; }
+    }
+}
