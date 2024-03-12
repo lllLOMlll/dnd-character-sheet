2e6283b56c97d2de9710cbcc9edd4927a23d3472
|||LOM||| <louis.olivier.major@gmail.com>
2024-03-12 14:00:53 -0400

    Initial commit

---
+++ Services/IClassService.cs
@@ -0,0 +1,16 @@
+﻿namespace CharacterSheetDnD.Services
+{
+    public interface IClassService
+    {
+        IEnumerable<string> GetAvailableClasses();
+    }
+
+    public class ClassService : IClassService
+    {
+        public IEnumerable<string> GetAvailableClasses()
+        {
+            // This list could come from a database, configuration file, or be statically defined
+            return new List<string> { "Wizard", "Fighter", "Rogue", "Cleric", "Paladin", "Ranger", "Warlock", "Barbarian", "Sorcerer", "Druid", "Monk", "Bard" };
+        }
+    }
+}
