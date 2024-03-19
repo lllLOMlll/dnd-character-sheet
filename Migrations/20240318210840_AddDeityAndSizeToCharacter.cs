9308be825438c8369205bf46afb5e339021fd7bc
|||LOM||| <louis.olivier.major@gmail.com>
2024-03-19 15:32:41 -0400

    Add new migrations for cascading deletes and character attributes

---
+++ Migrations/20240318210840_AddDeityAndSizeToCharacter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CharacterSheetDnD.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeityAndSizeToCharacter : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Deity",
+                table: "Characters",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Size",
+                table: "Characters",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deity",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "Size",
+                table: "Characters");
+        }
+    }
+}
