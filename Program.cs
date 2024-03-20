44304e8ed3fe979bded6d0d202284e5b92a42f38
|||LOM||| <louis.olivier.major@gmail.com>
2024-03-20 16:50:04 -0400

    Add AZURE_AD_TENANT_ID line

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var azureAdTenantId = Environment.GetEnvironmentVariable("AZURE_AD_TENANT_ID");
+
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
