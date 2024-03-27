45d3e0d05e4b7c7553d8c118b67ad13cb2d3ae19
|||LOM||| <louis.olivier.major@gmail.com>
2024-03-27 09:41:12 -0400

    Create a mehod to update Maxi Hit points

---
+++ Controllers/HitPointsController.cs
@@ -0,0 +1,45 @@
+﻿using CharacterSheetDnD.Data;
+using CharacterSheetDnD.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CharacterSheetDnD.Controllers
+{
+	public class HitPointsController : Controller
+	{
+		private readonly ApplicationDbContext _context;
+
+		public HitPointsController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		[HttpPost]
+		[Authorize]
+		[ValidateAntiForgeryToken]
+		[Route("my-character/update-maximum-hit-points")]
+		public async Task<IActionResult> UpdateMaximumHitPoints(int id, int maximumHitPoints)
+		{
+			var character = await _context.Characters
+													.Include(c => c.CharacterHealth)
+													.SingleOrDefaultAsync(c => c.CharacterID == id);
+
+			
+			if (character == null) 
+			{
+			 return NotFound($"Character with ID {id} not found.");
+			}
+
+			if (character.CharacterHealth == null)
+			{
+				character.CharacterHealth = new CharacterHealth { CharacterID = id };
+			}
+
+			character.CharacterHealth.MaximumHitPoints = maximumHitPoints;
+			await _context.SaveChangesAsync();
+
+			return RedirectToAction("DisplayCharacter", "MyCharacter", new { id = character.CharacterID });
+		}
+	}
+}
