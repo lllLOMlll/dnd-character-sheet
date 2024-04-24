e310a95dc91069779b6f9d6b4a1e6fc8e8d28cc1
|||LOM||| <louis.olivier.major@gmail.com>
2024-04-24 13:51:14 -0400

    Deploy db to Azure test #11 - Erase all secrets. Put it back. Token in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,34 @@
+using Azure.Identity;
 using CharacterSheetDnD.Data;
 using CharacterSheetDnD.Models;
 using CharacterSheetDnD.Services;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlServer(connectionString));
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDbContext<ApplicationDbContext>((serviceProvider, options) =>
+{
+	var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+	var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+	var connection = new SqlConnection(connectionString);
+	var tokenCredential = new DefaultAzureCredential();
+	var token = tokenCredential.GetToken(new Azure.Core.TokenRequestContext(new[] { "https://database.windows.net/.default" })).Token;
+	connection.AccessToken = token;
+
+	options.UseSqlServer(connection);
+});
+
+
 
 builder.Services.AddSession(options => {
 	options.IdleTimeout = TimeSpan.FromMinutes(300); // Set session timeout as needed
