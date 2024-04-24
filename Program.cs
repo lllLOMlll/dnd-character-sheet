9d805c95aaaa3412182c59c5bba01465e6368383
|||LOM||| <louis.olivier.major@gmail.com>
2024-04-24 13:57:15 -0400

    Deploy db to Azure test #13 - No Token in Program.cs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-Add services to the container.
+//Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 	options.UseSqlServer(connectionString));
